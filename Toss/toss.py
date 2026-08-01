f50c6362997b9cbadf9652a28598009a1a8f9193
shashank sul <shashanksul725@gmail.com>
2026-08-01 19:23:35 +0530

    add countdown

--- Toss/toss.py
+++ Toss/toss.py
@@ -13,4 +13,11 @@ if st.button("Toss"):
         placeholder.markdown(f'## 🪙 {result}') 
         time.sleep(0.1)
 
-    st.success(f'Final Result: 🪙 {result}')
+    # st.success(f'Final Result: 🪙 {result}')
+    countdown = st.empty()
+    for i in range(5, 0, -1):
+        countdown.info(f'Restarting in {i} seconds')
+        time.sleep(1)
+    countdown.empty()
+    st.rerun()
+
