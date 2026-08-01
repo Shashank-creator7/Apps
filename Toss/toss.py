4da8368b9c26507bec8259540f99e732e9e4bfce
shashank sul <shashanksul725@gmail.com>
2026-08-01 19:32:49 +0530

    Refactor coin toss and dice roll logic for improved structure and readability

--- Toss/toss.py
+++ Toss/toss.py
@@ -1,23 +1,41 @@
 import random
 import streamlit as st
 import time
-
-st.markdown(
+choice = st.sidebar.selectbox("Menu",menu=["Toss The Coin", "Roll The Dice"])
+if choice == "Toss The Coin":
+    st.markdown(
         "<h3 style='font-family:Verdana; color:#4dd0e1; font-size:px;'>🪙 TOSS THE COIN</b> </h3>",
         unsafe_allow_html=True)
 
-if st.button("Toss"):
-    placeholder = st.empty()
-    for i in range(10):
-        result = random.choice(["Heads", "Tails"])
-        placeholder.markdown(f'## 🪙 {result}') 
-        time.sleep(0.1)
+    if st.button("Toss"):
+        placeholder = st.empty()
+        for i in range(10):
+            result = random.choice(["Heads", "Tails"])
+            placeholder.markdown(f'## 🪙 {result}') 
+            time.sleep(0.1)
 
-    st.success(f'Final Result: 🪙 {result}')
-    countdown = st.empty()
-    for i in range(5, 0, -1):
-        countdown.warning(f'Restarting in {i} seconds')
-        time.sleep(1)
-    countdown.empty()
-    st.rerun()
+        st.success(f'Final Result: 🪙 {result}')
+        countdown = st.empty()
+        for i in range(5, 0, -1):
+            countdown.warning(f'Restarting in {i} seconds')
+            time.sleep(1)
+        countdown.empty()
+        st.rerun()
 
+if choice == "Roll The Dice":
+    st.markdown(
+            "<h3 style='font-family:Verdana; color:#4dd0e1; font-size:px;'>🪙 Roll The Dice</b> </h3>",
+            unsafe_allow_html=True)
+    if st.button("Roll"):
+        placeholder = st.empty()
+        for i in range(10):
+            result = random.randint(1, 6)
+            placeholder.markdown(f'## 🎲 {result}')
+            time.sleep(0.1)
+        st.success(f'Final Result: 🪙 {result}')
+        countdown = st.empty()
+        for i in range(5, 0, -1):
+            countdown.warning(f'Restarting in {i} seconds')
+            time.sleep(1)
+        countdown.empty()
+        st.rerun()
